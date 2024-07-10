7b5c3dce75b87c151041e42e6d552537fc72616f
HasinaAndrianasinoro16 <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2024-07-10 10:11:09 +0300

    Admin authebtification et migration

---
+++ Admin/app/Http/Middleware/AdminAuth.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        if (Auth::check() && Auth::user()->usertype == 1) {
+            return redirect('/Erreur')->with('error', "Vous n'avez pas accès à cette page.");
+        }
+
+        return $next($request);
+    }
+}
--- data.sql
+++ data.sql
@@ -1,4 +1,6 @@
 create database camppenal;
 \c camppenal
 
--- user creer par la 
+-- user creer par la fonctionnalite de breeze
+-- ajout de la table user type pour l'authentification et pour l'acces au site 
+
