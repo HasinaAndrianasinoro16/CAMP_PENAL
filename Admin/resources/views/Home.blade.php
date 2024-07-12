606ff48e699124eb3f8614ef34ef48fd3e297b04
HasinaAndrianasinoro16 <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2024-07-12 11:24:24 +0300

    dashboard et script sql

--- Admin/resources/views/Home.blade.php
+++ Admin/resources/views/Home.blade.php
@@ -1,3 +1,106 @@
 @extends('Dashboard')
 @section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="overview-wrap">
+                <h2 class="title-1">Tableau de bord</h2>
+            </div>
+            <div class="py-3"></div>
+        </div>
+        <div class="overview-chart" style="display: none"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+            <canvas id="widgetChart2" height="345" style="display: block; width: 340px; height: 115px;" width="1020" class="chartjs-render-monitor"></canvas>
+        </div>
+        <div class="row m-t-25">
+            <div class="col-sm-6 col-lg-3">
+                <div class="overview-item overview-item--c1">
+                    <div class="overview__inner">
+                        <div class="overview-box clearfix">
+                            <div class="icon">
+                                <i class="zmdi zmdi-account-o"></i>
+                            </div>
+                            <div class="text">
+                                <h2>10368</h2>
+                                <span>Utilisateur D.I.R.A.P</span>
+                            </div>
+                        </div>
+                        <div class="overview-chart"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+                            <canvas id="widgetChart2" height="345" style="display: block; width: 340px; height: 115px;" width="1020" class="chartjs-render-monitor"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="overview-item overview-item--c2">
+                    <div class="overview__inner">
+                        <div class="overview-box clearfix">
+                            <div class="icon">
+                                <i class="zmdi zmdi-account-o"></i>
+                            </div>
+                            <div class="text">
+                                <h2>388,688</h2>
+                                <span>Agent du ministere</span>
+                            </div>
+                        </div>
+                        <div class="overview-chart"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+                            <canvas id="widgetChart2" height="345" style="display: block; width: 340px; height: 115px;" width="1020" class="chartjs-render-monitor"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="overview-item overview-item--c3">
+                    <div class="overview__inner">
+                        <div class="overview-box clearfix">
+                            <div class="icon">
+                                <i class="fas fa-leaf"></i>
+                            </div>
+                            <div class="text">
+                                <h2>1,086</h2>
+                                <span>Camp penal</span>
+                            </div>
+                        </div>
+                        <div class="overview-chart"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+                            <canvas id="widgetChart2" height="345" style="display: block; width: 340px; height: 115px;" width="1020" class="chartjs-render-monitor"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+{{--            <div class="col-sm-6 col-lg-3">--}}
+{{--                <div class="overview-item overview-item--c4">--}}
+{{--                    <div class="overview__inner">--}}
+{{--                        <div class="overview-box clearfix">--}}
+{{--                            <div class="icon">--}}
+{{--                                <i class="zmdi zmdi-money"></i>--}}
+{{--                            </div>--}}
+{{--                            <div class="text">--}}
+{{--                                <h2>$1,060,386</h2>--}}
+{{--                                <span>total earnings</span>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="overview-chart"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>--}}
+{{--                            <canvas id="widgetChart4" height="396" style="display: block; width: 340px; height: 132px;" width="1020" class="chartjs-render-monitor"></canvas>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+        </div>
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <strong class="h3">Details</strong>
+                    </div>
+                    <p class="text-muted m-b-15">
+                        Dans cette page vous pouvez voir
+                        <code>Utilisateurs D.I.R.A.P</code>
+                        qui sont les responsables de chaque camp penal et
+                        <code>Agent Ministere</code>
+                        qui sont les responsable de chaque action
+                        <code>Camp penal</code>
+                        le nombre de camp penal enregistre.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- data.sql
+++ data.sql
@@ -2,5 +2,70 @@ create database camppenal;
 \c camppenal
 
 -- user creer par la fonctionnalite de breeze
--- ajout de la table user type pour l'authentification et pour l'acces au site 
+-- ajout de la table user type pour l'authentification et pour l'acces au site
 
+--les differents type de sol a madagascar favorable au culture
+create table sol(
+    id serial primary key
+    nom varchar(50)
+);
+
+-- liste des cultures pouvant etre planter dans un sol donnees
+create sequence seqculture increment by 1;
+create table culture(
+    id varchar(255) primary key,
+    nom varchar(50),
+    prixunitaire numeric(10,2),
+    sol int references sol(id)
+);
+
+--les provinces de madagascar
+create table province(
+    id serial primary key,
+    nom varchar(50)
+);
+
+--les utilisateurs par province
+create table ProvinceUtilisateur(
+    id serial,
+    users int references users(id),
+    province int references province(id)
+);
+
+--liste des camp penal
+create sequence seqcamp increment by 1; 
+create table camp(
+    id varchar(255),
+    nom varchar(50),
+    supeficie numeric(10,2),
+    province int references province(id),
+    longitude decimal,
+    lattitude decimal
+);
+
+-- liste des cultures stocke en general
+create table stockculture(
+    id serial primary key,
+    culture(id),
+    quantite numeric(10,2), --en kilogramme
+    datestock date,
+    etat int -- 0 pour entrer et 1 pour sortie // la quantite sortie represente les cultures donnees pour etre revendue tandis que l'entrer sont les cultures destine a etre consomner par les prisonniers
+);
+
+
+--liste des culture existant par camp
+create table campculture(
+    id serial,
+    camp varchar(255) references camp(id),
+    stock int references stockculture(id)
+);
+
+--=========================== INSERTION==============================
+INSERT INTO province (nom) VALUES
+('Antananarivo'),
+('Toamasina'),
+('Antsiranana'),
+('Mahajanga'),
+('Fianarantsoa'),
+('Toliara');
+--============================== View et fonction ===============================
