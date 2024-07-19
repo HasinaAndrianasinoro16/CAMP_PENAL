cc193fa43789bb4694902c85e0cc20da15603892
HasinaAndrianasinoro16 <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2024-07-19 08:44:51 +0300

    Update auth.php

--- Admin/routes/auth.php
+++ Admin/routes/auth.php
@@ -12,8 +12,8 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-//    Route::get('register', [RegisteredUserController::class, 'create'])
-//                ->name('tsisyltyhah');
+////    Route::get('register', [RegisteredUserController::class, 'create'])
+////                ->name('non');
 //
 //    Route::post('register', [RegisteredUserController::class, 'store']);
 
