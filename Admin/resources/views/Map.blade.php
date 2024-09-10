d3655675aff5f35183525105be19ea0f9ff74b91
HasinaAndrianasinoro16 <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2024-09-10 15:52:15 +0300

    manoro v2

--- Admin/resources/views/Map.blade.php
+++ Admin/resources/views/Map.blade.php
@@ -21,6 +21,7 @@
         @endif
         <div class="col-lg-12">
             <div class="row">
+                    <div class="col-lg-12">
                         <div class="card">
                             <div class="card-body">
                                 <div class="card-title h3">Liste camp penal</div>
